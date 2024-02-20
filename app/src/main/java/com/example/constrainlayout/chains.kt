1d1694cb725a46f0808be92c3985d80370501bdc
balakrishnasurya <90981476+balakrishnasurya@users.noreply.github.com>
2024-02-20 18:57:17 +0530

    first commit

---
+++ app/src/main/java/com/example/constrainlayout/chains.kt
@@ -0,0 +1,19 @@
+package com.example.constrainlayout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class chains : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chains)
+        val profilebtn = findViewById<Button>(R.id.button5)
+
+        profilebtn.setOnClickListener {
+            val intent = Intent(this,consLayout::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/constrainlayout/consLayout.kt
@@ -0,0 +1,11 @@
+package com.example.constrainlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class consLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cons_layout)
+    }
+}
